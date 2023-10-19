37d206b33f84db854a2e17d828ef8fc1b557e804
Valère Dunand <valere.d@gmail.com>
2023-10-19 21:19:58 +0200

    Add autocomplete suggestions to freetext input

--- inputFreeText.js
+++ inputFreeText.js
@@ -13,6 +13,9 @@ export default {
 
   template: `<div class="filter-criteria-input">
     <input type="text" v-model="searchValue" ref="input"> <button @click="valid">OK</button>
+    <div v-for="suggestion in suggestions">
+      <div @click="pickSuggestion(suggestion)">{{ suggestion }}</div>
+    </div>
   </div>`,
 
   mounted() {
@@ -20,10 +23,29 @@ export default {
   },
 
   methods: { 
+    pickSuggestion: function(value) {
+      this.searchValue = value;
+      this.valid();
+    },
+
     valid: function() {
       this.valueProp.value = this.searchValue;
       this.searchValue = '';
       this.$emit('changed', this.criteria);
+    },
+  },
+
+  computed: { 
+    suggestions: function() {
+      if (this.searchValue.trim() === '') {
+        return [];
+      }
+      if (!this.criteria.suggestions) {
+        return [];
+      }
+      return this.criteria.suggestions.filter(suggestion => {
+        return suggestion.toLowerCase().includes(this.searchValue.toLowerCase());
+      });
     },
   },
 
